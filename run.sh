ad6156e06857439f890de564763003d188b3e53c
Raffael Schneider <77717645+raffaelschneider@users.noreply.github.com>
2022-03-24 13:45:28 +0100

    fixed some minor issues

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 readonly IMAGE_TAG=b-nova-techup/haproxy:0.0.0
 
 frontend_bind="8585"
-backend_ip="127.0.0.1"
+backend_ip="echo0"
 backend_port="80"
 
 docker build --quiet=true --tag $IMAGE_TAG .
@@ -15,15 +15,17 @@ docker run --rm -it -e FRONTEND_BIND=$frontend_bind -e BACKEND_IP=$backend_ip -e
 echo "Starting proxy and echo-server..."
 
 docker network create --driver=bridge cluster
-docker run --rm -d --name echo --net cluster ealen/echo-server:latest
+docker run --rm -d --name $backend_ip --net cluster ealen/echo-server:latest
 docker run --rm -d --name proxy --net cluster -p $frontend_bind:$frontend_bind -p 8404:8404 -e FRONTEND_BIND=$frontend_bind -e BACKEND_IP=$backend_ip -e BACKEND_PORT=$backend_port $IMAGE_TAG
 
 set -B
 for i in {1..10}; do
-  curl -s -k 'GET' -H 'header info' -b 'body' '$backend_ip:'$frontend_bind'/id='$i
-  sleep 1s
+  echo "Initiating request $i"
+  curl -s -k -i 'GET' -H 'header info' -b 'body' 'localhost:'$frontend_bind'/id='$i
+  sleep 1
+  echo "\n\n"
 done
 
-docker stop echo
+docker stop $backend_ip
 docker stop proxy
 docker network rm cluster
